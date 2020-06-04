481fd7af9417a69e0aa64c04f7ba727a036855d4
alfredrafael <alfredrafael@gmail.com>
2020-06-04 01:46:30 -0400

    Success

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import Fade from 'react-reveal/Fade';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import logo from "../../logo.svg";
 import HeroSlider from '../HeroSlider';
 import Navbar from '../Navbar';
+
 import AwesomeSlider from 'react-awesome-slider';
 import 'react-awesome-slider/dist/styles.css';
+import withAutoplay from 'react-awesome-slider/dist/autoplay';
+
 
 const Home = () => {
 
+
+  const AutoplaySlider = withAutoplay(AwesomeSlider);
+
+
   const slider = (
-    <AwesomeSlider>
+    <AwesomeSlider bullets={false}>
       <div data-src="https://alfredorafael.com/wp-content/uploads/2020/06/20.jpeg" />
       <div data-src="https://alfredorafael.com/wp-content/uploads/2020/06/3.jpeg" />
-      <div data-src="/path/to/image-2.jpg" />
+      <div data-src='https://alfredorafael.com/wp-content/uploads/2020/06/23-3.jpeg'/>
+      <div data-src='https://alfredorafael.com/wp-content/uploads/2020/06/23-3-scaled.jpeg'/>
+      <div data-src='https://alfredorafael.com/wp-content/uploads/2020/06/22-scaled.jpeg'/>
+      <div data-src="https://alfredorafael.com/wp-content/uploads/2020/06/21-scaled.jpeg"/>
     </AwesomeSlider>
   );
   return(
@@ -22,8 +31,6 @@ const Home = () => {
     <div style={{height: '100vh'}}>
     <h1> Welcome </h1>
    {slider}
-);
-
     </div>
   </div>
 )};
